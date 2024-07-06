46f8a6aed54feab833c037e4fc15ecca9eba5d49
Guilherme <guilhermecarvalhobao@gmail.com>
2024-07-06 01:08:36 -0300

    Teste

--- src/components/hero.vue
+++ src/components/hero.vue
@@ -10,7 +10,7 @@
         </div>
         <div class="flex lg:hidden">
           <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-400" @click="mobileMenuOpen = true">
-            <span class="sr-only">Open main menu</span>
+            <span class="sr-only">Open main menuv</span>
             <Bars3Icon class="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
